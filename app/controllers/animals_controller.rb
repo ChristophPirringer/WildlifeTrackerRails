9a283533ab574f88285b132e1769e67a9284addf
Tony Navarro & Christoph Pirringer <student@epicodus.com>
2015-10-12 15:55:41 -0700

    complete CRUD-functionality for animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,6 +9,8 @@ class AnimalsController < ApplicationController
     render :show
   end
 
+
+
   def new
     @animal = Animal.new
     render :new
@@ -22,6 +24,31 @@ class AnimalsController < ApplicationController
       render :new
     end
   end
+
+
+
+  def edit
+    @animal = Animal.find(params[:id])
+    render :edit
+  end
+
+  def update
+     @animal= Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animals_path
+    else
+      render :edit
+    end
+  end
+
+
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path
+  end
+
 
   private
 
